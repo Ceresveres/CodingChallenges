d10267a9aa1e278654da7dfcd46b1c1e94a47449
Ceresveres <ceresveres@gmail.com>
2021-12-26 19:44:29 -0800

    Create tribonaci.cpp

---
+++ tribonaci.cpp
@@ -0,0 +1,12 @@
+int tribonacci(int n) { 
+    if (n == 0) return 0;
+    int t0 = 0, t1 = 1, t2 = 1;
+    
+    for (int i=0; i<n-2; i++) {
+        int tn = t0 + t1 + t2;
+        t0 = t1;
+        t1 = t2; 
+        t2 = tn;
+    }
+    return t2;
+}
