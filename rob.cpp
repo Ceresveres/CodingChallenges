3bfdfa9f80f2c83042538c4457315f456e2a2c8a
Ceresveres <ceresveres@gmail.com>
2021-12-27 16:29:06 -0800

    Update rob.cpp

--- rob.cpp
+++ rob.cpp
@@ -11,3 +11,20 @@ int rob(vector<int>& nums) {
     }
     return prev1;
 }
+
+// same as above excepts treats arrayvector as a circle
+int rob(vector<int>& nums) {
+    int n = nums.size(); 
+    if (n < 2) return n ? nums[0] : 0;
+    return max(robIterate(nums, 0, n - 2), robIterate(nums, 1, n - 1));
+}
+
+int robIterate(vector<int>& nums, int l, int r) {
+    int prev1 = 0, prev2 = 0;
+    for (int i = l; i <= r; i++) {
+        int temp = prev1;
+        prev1 = max(prev2 + nums[i], prev1);
+        prev2 = temp;
+    }
+    return prev1;
+}
